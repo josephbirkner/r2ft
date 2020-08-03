4cc944c4a86fe71b68a3990b8f4e7655cb079730
Joseph Birkner <joseph.birkner@tum.de>
2020-08-03 23:45:03 +0200

    ObjectHeader deserialisation.

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,18 +1,38 @@
+#![feature(macro_rules)]
+
 use std::vec;
 use std::io;
+pub use std::str::FromStr;
 
 pub mod fnv1a32;
 
 pub type Cursor = io::Cursor<Vec<u8>>;
 
-pub struct Error {
-    what: String
+pub enum SerializationResult {
+    Ok, Err(io::Error)
 }
 
 pub trait Serializable {
-    fn serialize(&self, cursor: &mut Cursor);
+    fn serialize(&mut self, cursor: &mut Cursor);
+    fn deserialize(&mut self, cursor: &mut Cursor) -> SerializationResult;
 }
 
-pub trait Deserializable<T> {
-    fn deserialize(&self, cursor: &mut Cursor) -> Result<T, Error>;
+macro_rules! read_u8 {
+    ($cursor:ident) => {match cursor.read_u8() { Ok(x) => x, Err(e) => return SerializationResult(Err(e)) };};
+}
+
+macro_rules! read_u16 {
+    ($cursor:ident) => {match cursor.read_u16() { Ok(x) => x, Err(e) => return SerializationResult(Err(e)) };};
+}
+
+macro_rules! read_u32 {
+    ($cursor:ident) => {match cursor.read_u32() { Ok(x) => x, Err(e) => return SerializationResult(Err(e)) };};
+}
+
+macro_rules! read_u64 {
+    ($cursor:ident) => {match cursor.read_u64() { Ok(x) => x, Err(e) => return SerializationResult(Err(e)) };};
+}
+
+macro_rules! read_leb128 {
+    ($cursor:ident) => {match leb128::read::unsigned(cursor) { Ok(x) => x, Err(e) => return SerializationResult(Err(e))}; };
 }
--- src/transport/frame.rs
+++ src/transport/frame.rs
@@ -1,9 +1,8 @@
 use std::vec;
 use crate::common::*;
-use byteorder::{NetworkEndian, NativeEndian, WriteBytesExt};
-use std::io::{Write, Seek};
+use byteorder::{NetworkEndian, NativeEndian, WriteBytesExt, ReadBytesExt};
+use std::io::{Write, Seek, SeekFrom, Error};
 use leb128;
-use std::io::SeekFrom;
 use crate::common::fnv1a32;
 
 /////////////////////////////////
@@ -31,15 +30,34 @@ impl Serializable for MessageFrame {
         cursor.write_u64::<NetworkEndian>(self.sid);
         cursor.write_u8(self.tlvs.len() as u8);
         for tlv in &self.tlvs {
-            match tlv {
-                TransportTlv::ObjectHeader(x) => {
-                    x.serialize(cursor);
-                }
-            }
+            tlv.serialize(cursor);
         }
         let end = cursor.position();
         let checksum = fnv1a32::Fnv32a::hash(cursor, start, end);
         cursor.write_u32::<NetworkEndian>(checksum);
+    }
+
+    fn deserialize(&mut self, cursor: &mut Cursor) -> SerializationResult {
+        self.version = read_u8!(cursor);
+        self.sid = read_u64!(cursor);
+        self.tlvs = Vec::new();
+        let mut num_tlvs = read_u8!(cursor);
+        while num_tlvs > 0 {
+            let tlv_type = match cursor.read_u8() {
+                Ok(typeCode) => typeCode,
+                Err(e) => return SerializationResult::Ok // Err in this case means eof, that's ok
+            };
+            let tlv = match tlv_type {
+                tlv_type if tlv_type == (TransportTlvTypeCode::ObjectHeader as u8) => TransportTlv(TransportTlv::ObjectHeader),
+                _ => return SerializationResult::Err(Error("Unknown object type code!"))
+            };
+            tlv.deserialize(cursor);
+            self.tlvs.push(tlv);
+            num_tlvs -= 1;
+        }
+
+        // FIXME: Compare hash!
+        SerializationResult(Ok)
     }
 }
 
@@ -52,6 +70,25 @@ pub enum TransportTlv {
 
 enum TransportTlvTypeCode {
     ObjectHeader = 0x51
+}
+
+impl Serializable for TransportTlv {
+    fn serialize(&self, cursor: &mut Cursor) {
+        match self {
+            TransportTlv::ObjectHeader(x) => {
+                x.serialize(cursor);
+            }
+        }
+    }
+
+    fn deserialize(&mut self, cursor: &mut Cursor) -> SerializationResult {
+        match self {
+            TransportTlv::ObjectHeader(x) => {
+                x.deserialize(cursor);
+            }
+        }
+        SerializationResult(Ok)
+    }
 }
 
 /////////////////////////////////
@@ -91,6 +128,34 @@ impl Serializable for ObjectHeader {
         cursor.write_u16::<NetworkEndian>((length - 2) as u16); // -2 bc. of length-field
         cursor.seek(SeekFrom::Current(length as i64));
     }
+
+    fn deserialize(&mut self, cursor: &mut Cursor) -> SerializationResult {
+        // NOTE: TLV Type is already parsed!
+        let mut length = read_u16!(cursor);
+        let pos = cursor.position();
+
+        // Read object header content
+        self.object_id = read_u64!(cursor);
+        self.n_chunks = read_leb128!();
+        self.ack_req = match read_u8!(cursor) {
+            0b1000_0000 => true,
+            _ => false,
+        };
+        self.object_type = read_u8!(cursor);
+        let mut num_fields = read_u8!(cursor);
+        while num_fields > 0 {
+            self.fields.push(ObjectFieldDescription());
+            self.fields.last().deserialize(cursor);
+            num_fields -= 1;
+        }
+
+        // Determine & write length field
+        let final_length = cursor.position() - pos;
+        if length != final_length {
+            return SerializationResult::Err(Error("Unknown object type code!"));
+        }
+        SerializationResult(Ok)
+    }
 }
 
 /////////////////////////////////
@@ -104,10 +169,16 @@ pub struct ObjectFieldDescription {
     length: ChunkId // in nr. of chunks
 }
 
-impl Serializable for ObjectFieldDescription {
+impl Serializable for ObjectFieldDescription{
     fn serialize(&self, cursor: &mut Cursor) {
         cursor.write_u8(self.field_type);
         leb128::write::unsigned(cursor, self.length);
+    }
+
+    fn deserialize(&mut self, cursor: &mut Cursor) -> SerializationResult {
+        self.field_type = read_u8!(cursor);
+        self.length = read_leb128!(cursor) as ChunkId;
+        SerializationResult(Ok)
     }
 }
 
