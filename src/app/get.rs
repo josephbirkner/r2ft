4a92126f6b27bab6a33ea688cef7a6e987f896da
Joseph Birkner <joseph.birkner@tum.de>
2020-08-12 00:11:07 +0200

    Fix borrowing

--- src/app/get.rs
+++ src/app/get.rs
@@ -1,55 +1,83 @@
+use crate::app::frame::*;
+use crate::options::Options;
+use crate::transport::client;
+use crate::transport::connection::*;
+use crate::transport::jobs::*;
 use log::info;
 use std::net::SocketAddr;
-use crate::options::Options;
-use crate::transport::connection::*;
-use crate::transport::client;
-use crate::transport::jobs::*;
-use crate::app::frame::*;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+#[derive(PartialEq, Eq)]
+enum State {
+    Startup,
+    Connected,
+    TransferInProgress,
+    Finished,
+}
+
+struct StateMachine {
+    state: State,
+}
+
+impl StateMachine {
+    fn new() -> Self {
+        StateMachine {
+            state: State::Startup,
+        }
+    }
+
+    fn connected(&mut self) {
+        self.state = State::Connected;
+    }
+
+    fn transfer_in_progress(&mut self) {
+        self.state = State::TransferInProgress;
+    }
+
+    fn finished(&mut self) {
+        self.state = State::Finished;
+    }
+
+    fn is_finished(&mut self) -> bool {
+        self.state == State::Finished
+    }
+}
 
 /// Run client for file retrieval.
-pub fn get(
-    opt: Options,
-    socket_addr: SocketAddr,
-    files: Vec<&str>,
-) -> std::result::Result<(), ()>
-{
+pub fn get(opt: Options, socket_addr: SocketAddr, files: Vec<&str>) -> std::result::Result<(), ()> {
     // Announce client startup
-    info!(
-        "File client startet with {} for socket address {} and file(s):",
+    let mut s = format!(
+        "File client startet with {} for socket address {} and file(s) '",
         opt, socket_addr
     );
-    for file in &files {
-        info!(" {}", file);
-    }
-    info!("\n");
 
-    #[derive(PartialEq,Eq)]
-    enum State {
-        Startup,
-        Connected,
-        TransferInProgress,
-        Finished
+    for f in files {
+        s = format!("{} {}", s, f);
     }
 
-    let mut state = State::Startup;
+    info!("{} '", s);
+
+    let state_machine = Arc::new(Mutex::new(StateMachine::new()));
+
     let mut next_object_id = 1;
 
-    let incoming_object_handler = |recv_job| {
+    let incoming_object_handler = |recv_job| {};
 
-    };
-
-    let timeout_handler = || {
-        state = State::Finished;
+    let state_machine_for_timeout_handler = Arc::clone(&state_machine);
+    let timeout_handler = move || {
+        state_machine_for_timeout_handler.lock().unwrap().finished();
     };
 
     let connection = client::connect(
         socket_addr,
         Box::new(incoming_object_handler),
-        Box::new(timeout_handler));
-    state = State::Connected;
+        Box::new(timeout_handler),
+    );
 
-    while state != State::Finished {
-    }
+    state_machine.lock().unwrap().connected();
+
+    while !state_machine.lock().unwrap().is_finished() {}
 
     unimplemented!();
     Ok(())
--- src/transport/server.rs
+++ src/transport/server.rs
@@ -3,10 +3,11 @@ use crate::common::udp::Socket;
 use crate::transport::connection::*;
 
 /// Is called by the transport layer to inform the application
-/// about new connections. The server-application then returns its
-/// ObjectListener for that connection.
+/// about a new connection coming from `incoming`. The server-
+/// application then uses that SocketAddr to get a `Connection`
+/// via `crate::transport::client::connect()`.
 /// TODO: fn(incoming: SocketAddr) and then app shall call connect on that addr
-pub type ConnectionListener = fn (incoming: Connection) -> ObjectListener;
+pub type ConnectionListener = fn (incoming: SocketAddr) -> ();
 
 pub struct Listener {
     socket: UdpSocket,
@@ -18,28 +19,31 @@ impl Listener {
     /// Install a ConnectionListener to be called for each new
     /// incoming connection. Will listen at `bind`.
     /// Non-blocking.
-    pub fn listen(bind: SocketAddr, callback: ConnectionListener) -> Listener {
+    pub fn new(bind: SocketAddr, callback: ConnectionListener) -> Listener {
         let socket: UdpSocket = UdpSocket::bind(bind).expect("Could not bind to Socket.");
-        socket.set_nonblocking(true);
+        socket.set_nonblocking(true).unwrap();
         Listener {
             socket,
             callback
         }
     }
 
-    pub fn grant_cpu(&self) {
+    /// non-blocking
+    pub fn listen_once(&self) {
         let mut buf: [u8; 10] = [0; 10];
-        if let Ok((n_bytes, src)) = self.socket.peek_from(&mut buf) {
-            self.socket.connect(src);
-            let conn = Connection {
-                send_jobs: Vec::new(),
-                recv_jobs: Vec::new(),
-                accept_callback: |a| {},
-                timeout_callback: || {},
-                socket: self.socket,
-                dest: src
-            };
-            self.callback(conn);
+        if let Ok((_n_bytes, src)) = self.socket.peek_from(&mut buf) {
+//            self.socket.connect(src).unwrap();
+//            let conn = Connection {
+//                send_jobs: Vec::new(),
+//                recv_jobs: Vec::new(),
+//                accept_callback: Box::new(|a| {}),
+//                timeout_callback: Box::new(|| {}),
+//                socket: self.socket.try_clone().unwrap(),
+//                dest: src
+//            };
+            let connection_listener = *self.callback;
+            connection_listener(src);
+            todo!();
         }
     }
 }
