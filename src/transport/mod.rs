bb647ecae20ae05a1904e1bc77a82973c78be3b7
Peter Okelmann <okelmann@in.tum.de>
2020-08-11 10:08:15 +0200

    incorporate feedback
    - remove Object.get_size()
    - fix compiler errors
    - add todo!s
    
    and add impl Object for Receiving Object

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -8,8 +8,6 @@ use frame::*;
 
 pub const CHUNKSIZE: usize = 512; ///maximum chunksize in bytes.
 
-type Stub = String;
-
 //////////////////////////
 // Interfaces which define, what an application can send
 
@@ -18,9 +16,6 @@ type Stub = String;
 /// receive and provide chunks of this object. 
 pub trait Object<T: ObjectField> {
     fn get_type(&self) -> ObjectType;
-
-    /// size in chunks.
-    fn get_size(&self) -> usize;
 
     /// An Object must contain at least one field.
     fn get_field(&self, index: u32) -> T;
@@ -35,7 +30,41 @@ pub trait ObjectField {
     fn get_type(&self) -> ObjectFieldType;
 
     /// size in chunks.
-    fn get_size() -> usize;
+    fn get_size(&self) -> usize;
+}
+
+//////////////////////////
+// Object and ObjectField implementation used by the transport 
+// layer to offer information to the application.
+
+struct ReceivingObject {
+}
+
+impl Object<ReceivingObjectField> for ReceivingObject {
+    fn get_type(&self) -> ObjectType {
+        todo!();
+    }
+    
+    fn get_field(&self, index: u32) -> ReceivingObjectField {
+        todo!();
+    }
+
+    fn get_field_count(&self) -> usize {
+        todo!();
+    }
+}
+
+struct ReceivingObjectField {
+}
+
+impl ObjectField for ReceivingObjectField {
+    fn get_type(&self) -> ObjectFieldType {
+        todo!();
+    }
+
+    fn get_size(&self) -> usize {
+        todo!();
+    }
 }
 
 //////////////////////////
@@ -49,6 +78,10 @@ pub struct Connection<T: ObjectField, U: Object<T>> {
     /// layer to pass the chunks of this Object on.
     accept_callback: fn (receiver: ObjectReceiver<T, U>) -> 
         Option<fn (chunk: &Vec<u8>, id: ChunkId) -> ()>,
+
+    /// required to suppress "unused" errors for T and U
+    placeholder_state: std::marker::PhantomData<U>,
+    placeholder_state_2: std::marker::PhantomData<T>,
 }
 
 impl<T: ObjectField, U: Object<T>> Connection<T, U>{
@@ -59,6 +92,8 @@ impl<T: ObjectField, U: Object<T>> Connection<T, U>{
 
         Connection::<T, U>{
             accept_callback: accept_callback,
+            placeholder_state: std::marker::PhantomData::default(),
+            placeholder_state_2: std::marker::PhantomData::default(),
         }
     }
 
@@ -83,33 +118,35 @@ pub struct ObjectSender {
 
 impl ObjectSender {
     pub fn abort_sending(&mut self) {
-        // TODO
+        todo!();
     }
 
     pub fn skip_to(&mut self, chunk: ChunkId) {
-        // TODO
+        todo!();
     }
 
-    /// optionally TODO:
     pub fn progress(&self) -> f64 {
-        0.0
+        unimplemented!();
     }
 }
 
 pub struct ObjectReceiver<T: ObjectField, U: Object<T>> {
+    /// required to suppress "unused" errors for T and U
+    placeholder_state: std::marker::PhantomData<U>,
+    placeholder_state_2: std::marker::PhantomData<T>,
 }
 
 impl<T: ObjectField, U: Object<T>> ObjectReceiver<T, U> {
     /// Used to get metadata about the object.
     pub fn get_object(&self) -> U {
-        // TODO
+        todo!();
     }
 
     /// instead of returning None through the Connection.accept_callback 
     /// lambda function, the application may also abort receiving at 
     /// a later point. 
     pub fn abort_receiving(&mut self) {
-        // TODO
+        todo!();
     }
 }
 
